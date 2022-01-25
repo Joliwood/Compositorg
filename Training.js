afcec9bb2a02b64ec10310856f68e9e6917b399f
Joliwood <jolibois.contact@gmail.com>
2022-01-25 18:58:22 +0100

    Mise en place fonction html depuis le javascript

--- Training.js
+++ Training.js
@@ -55,31 +55,36 @@ let app2 = {
     // un blur pour la perte de focus
     // un let = mon information rentrée dans le placeholder
     let bloc4text = document.getElementById("infosend");
-    let azpo = document.getElementById("sendbutton");
     // un let pour attribuer le texte à respecter (qui est dans le html)
-    let respectText = "azpoazpo21";
+    let respectText = document.getElementById("torespect").innerHTML;
     // un let pour compter les erreurs, soit 0, 1 ou 2
-    let errorNum;
+    let nbErrors = 0;
     // une boucle if (si c'est pas bon, ajoute une class au placeholder pour ajouter une bordure rouge, si c'est bon, ça ajoute aussi une bordure mais verte)
-    if (respectText === bloc4text.value) {
-        // ajoute la class text-ok à l'id #infosend + supp l'autre
-        // else -> ajoute la classe text-faux à l'id #infosend + supp l'autre
- 
-        bloc4text.addEventListener('blur', (event) => {
-        console.log("Tu avances !");
-        });
-        
-    }
     
-    // une fonction globale qui s'appellera init qui se déclenchera au clic sur l'envoie à l'équipe
+        bloc4text.addEventListener('blur', () => {
+        if (respectText === bloc4text.value) {
+            document.getElementById("infosend").classList.remove("wronganswer");
+            document.getElementById("infosend").classList.add("goodanswer");
+            console.log("C'est une bonne réponse !");
+        } 
+        if (respectText !== bloc4text.value) {
+            document.getElementById("infosend").classList.remove("goodanswer");
+            document.getElementById("infosend").classList.add("wronganswer");
+            console.log("C'est une mauvaise réponse !");
+        }
+        });
+    
+    // une fonction globale qui se déclenchera au clic sur l'envoie à l'équipe
+        document.getElementById("sendbutton").addEventListener("click", () => {
     // une seconde boucle if (si l'userValue === azpoazpo21 alors le let compteur = 0, si c'est azpoazpo, let compteur = 1, else -> let compteur = 2)
     // une troisième boucle if (si c'est bon -> je sais pas encore, si c'est pas bon ça va créer une liste avec :)
     // compter le nombre d'erreurs, et créer une liste en fonction intégré dans le html entre le placeholder et le button
+        })
 
     }
 }
 
 
 
-document.getElementById("sendbutton").addEventListener("click", app2.init);
+document.addEventListener("DOMContentLoaded", app2.init);
 document.getElementById("guessnumbutton").addEventListener("click", app.init);
